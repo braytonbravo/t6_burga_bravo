eafda8b7462b16053469f5187b9ab1a61bea3c15
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:12:36 -0500

    se a entregado el ejercicio16

---
+++ bravo/parte2/ejercicio16.py
@@ -0,0 +1,36 @@
+import os
+#BOLETA DE VENTA
+#DECLARACION DE VARIABLES
+vendedor,precio_fierros,precio_alambres="",0,0.0
+
+vendedor=os.sys.argv[1]
+precio_fierros=int(os.sys.argv[2])
+precio_alambres=float(os.sys.argv[3])
+
+#PROCESSING
+precio_venta=round(precio_fierros+precio_alambres)
+
+#VERIFICADORES
+vendedor_sastifecho=(precio_venta>50)
+
+#OUTPUT
+print("#############################")
+print("#  FORMULA MATEMATICA       #")
+print("#############################")
+print("#")
+print("# Vendedor        : ", vendedor)
+print("# Precio de fierros: ", precio_fierros)
+print("# Precio de alambres: ", precio_alambres)
+print("# Precio de venta : ", precio_venta)
+print("##############################")
+
+#CONDICIONALES DOBLES
+#SI EL PRECIO DE VENTA SUPERA A LOS 50 SOLES EL VENDEDOR ESTARA SASTIFECHO
+if (vendedor_sastifecho==True):
+    print("vendedor sastifecho")
+
+#SI EL PRECIO DE VENTA NO SUPERA LOS 50 SOLES EL VENDEDOR PERDERA
+if (vendedor_sastifecho<50):
+    print("el vendedor a perdido")
+
+#fin_if
