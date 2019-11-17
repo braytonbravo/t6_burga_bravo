708bc277304c30c04f78cb3259d9756c8b034749
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:12:03 -0500

    se a entregado el ejercicio14

---
+++ bravo/parte2/ejercicio14.py
@@ -0,0 +1,36 @@
+import os
+#FORMULA FISICA
+#DECLARACION DE VARIABLES
+angulo,pi,tiempo=0.0,0.0,0.0
+
+angulo=float(os.sys.argv[1])
+pi=float(input("Ingrese pi:"))
+tiempo=float(input("ingrese tiempo:"))
+
+# PROCESSING
+velocidad_angular=round(angulo/tiempo)
+
+#VERIFICADORES
+velocidad_angular=(velocidad_angular>5)
+
+# OUTPUT
+print("################################")
+print("# FORMULA FISICA")
+print("################################")
+print("#")
+print("# El valor del angulo es : ", angulo)
+print("# El valor de  pi es     : ", pi)
+print("# El valor del tiempo es : ", tiempo)
+print("# La velocidad angular es: ", velocidad_angular )
+print("#################################")
+
+#CONDICIONAL DOBLE
+#si la velocidad angular resulta mayor a 5 sera un excelente resultado
+if (velocidad_angular==True):
+    print("excelente ejercicio")
+
+#si la velocidad angular resulta menos a 5 sera un ejercicio mal hecho
+if (velocidad_angular<5):
+    print("ejercicio mal hecho")
+
+#fin_if
