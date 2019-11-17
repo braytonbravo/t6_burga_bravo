dd8c174e619115ecf5990403eb8d25f42af5629a
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:06:27 -0500

    se a entregado el ejercicio3

---
+++ bravo/parte1/ejercicio3.py
@@ -0,0 +1,35 @@
+import os
+#BOLETA DE VENTA
+#DECLARACION DE VARIABLES
+cliente,arroz,manzana,aceite="",0.0,0.0,0.0
+
+
+cliente=os.sys.argv[1]
+arroz=float(os.sys.argv[2])
+manzana=float(os.sys.argv[3])
+aceite=float(os.sys.argv[4])
+
+#PROCESSING
+total=round(arroz+manzana+aceite)
+
+#VERIFICADOR
+cliente_sastifecho=(total>50)
+
+# OUTPUT
+print("#######################################")
+print("#    BODEGA - MI DANNA   ")
+print("#######################################")
+print("#")
+print("# cliente    : ", cliente)
+print("# arroz      : ", arroz)
+print("# manzana    : ", manzana)
+print("# aceite     : ", aceite)
+print("# total      : ", total)
+print("#######################################")
+
+#CONDICIONAL SIMPLE
+#SI EL CLIENTE LLEVA DE PRODUCTO MAS 50 SOLES SE LE DESCUENTA EL 10%
+if (cliente_sastifecho==True):
+    print("Descuento el 10%")
+#fin_if
+
