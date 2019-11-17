01da57b763c0e56990ddac09139571e603738261
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:16:21 -0500

    se a entregado el ejercicio27

---
+++ bravo/parte3/ejercicio27.py
@@ -0,0 +1,42 @@
+import os
+#BOLETA DE VENTA
+#DECLARACION DE LAS VARIABLES
+cliente,precio_de_licuadora,precio_de_lavadora,precio_del_televisor="",0.0,0.0,0.0
+
+cliente=os.sys.argv[1]
+precio_de_licuadora=float(os.sys.argv[2])
+precio_de_lavadora=float(os.sys.argv[3])
+precio_del_televisor=float(os.sys.argv[4])
+
+# PROCESSING
+precio_total=round(precio_de_licuadora+precio_de_lavadora+precio_del_televisor)
+
+#VERIFICADORES
+cliente_sastifecho=(precio_total>60)
+
+# OUTPUT
+print("############################################")
+print("#    PLAZA VEA   ")
+print("############################################")
+print("#")
+print("#  Cliente                   : ", cliente)
+print("# El precio de la licuadora es: s/.", precio_de_licuadora)
+print("# El precio de lavadora es    : s/.", precio_de_lavadora)
+print("# El precio del televisor es  : s/.", precio_del_televisor)
+print("# El precio total es          : s/.", precio_total)
+print("############################################")
+
+#CONDICIONALES MULTIPLES
+#SI EL CLIENTE COMPRA LOS PRODUCTOS A MAS DE 60 SOLES SE LLEVARA OTRO PRODUCTO ADICIONAL
+if (cliente_sastifecho==True):
+    print("ganaste un producto adicional")
+
+#SI EL CLIENTE COMPRA LOS PRODUCTOS A MENOS DE 60 SOLES NO TENDRA SU PRODUCTO ADICIONAL
+if (cliente_sastifecho<60):
+    print("no tendra su producto adicional")
+
+#SI EL CLIENTE COMPRA LOS PRODUCTOS IGUAL A 60 SOLES EL CLIENTE TIENE UN DESCUENTO ADICIONAL
+if (cliente_sastifecho==60):
+    print("tiene un descuento adicional")
+
+#fin_if
