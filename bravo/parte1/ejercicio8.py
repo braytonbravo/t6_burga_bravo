860a4b61a224741466ca6557f85cefc1e8205286
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:10:11 -0500

    se a entregado el ejercicio8

---
+++ bravo/parte1/ejercicio8.py
@@ -0,0 +1,33 @@
+import os
+#BOLETA DE VENTA
+#DECLARACION DE LAS VARIABLES
+cliente,precio_de_la_hibuprofeno,precio_de_omeprazol,precio_del_alcohol="",0,0,0.0
+
+cliente=os.sys.argv[1]
+precio_de_la_hibuprofeno=int(os.sys.argv[2])
+precio_de_omeprazol=int(os.sys.argv[3])
+precio_del_alcohol=float(os.sys.argv[4])
+
+# PROCESSING
+total_a_pagar=round(precio_de_la_hibuprofeno+precio_de_omeprazol+precio_del_alcohol)
+
+#VERIFICADORES
+cliente_complacido=(total_a_pagar<20)
+
+# OUTPUT
+print("#####################################")
+print("#  BOTICA - MI SALUD                #")
+print("#####################################")
+print("#")
+print("# Cliente                      : ", cliente)
+print("# El precio del hibuprofeno es : s/.", precio_de_la_hibuprofeno)
+print("# El precio del omeprazol es   : s/.", precio_de_omeprazol)
+print("# El precio del alcohol es     : s/.", precio_del_alcohol)
+print("# El total a pagar es          : s/.", total_a_pagar)
+print("#####################################")
+
+#CONDICIONALES SIMPLE
+#SI EL CLIENTE GASTA MENOS DE 20 SOLES NO TENDRA DESCUENTO
+if (cliente_complacido==True):
+    print("No tendra descuento")
+#fin_if
