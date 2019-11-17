b06362a0bfd7dc7a56fd9cba8278bde5f7744f7b
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-17 15:05:31 -0500

    se a entregado el ejercicio2

---
+++ bravo/parte1/ejercicio2.py
@@ -0,0 +1,34 @@
+import os
+#BOLETA DE NOTAS
+#DECLARACION DE LAS VARIABLES
+alumno,nota1,nota2,nota3="",0,0,0
+
+alumno=os.sys.argv[1]
+nota1=int(os.sys.argv[2])
+nota2=int(os.sys.argv[3])
+nota3=int(os.sys.argv[4])
+
+#PROCESSING
+promedio_ponderado=round(nota1+nota2+nota3)/3
+
+#VERIFICADOR
+alumno_satstifecho=(promedio_ponderado>10)
+
+#OUTPUT
+print("##############################")
+print("# BOLETAS DE NOTAS           #")
+print("##############################")
+print("#")
+print("# Alumno             : ", alumno)
+print("# Nota1              : ", nota1)
+print("# Nota2              : ", nota2)
+print("# Nota3              : ", nota3)
+print("# Promedio Ponderado : ", promedio_ponderado)
+print("##############################")
+
+#CONDICION SIMPLE
+#SI EL ALUMNO SACA EL PROMEDIO PONDERADO MAYOR QUE 10 ENTONCES ESTA APROBADO
+if (alumno_satstifecho==True):
+    print("Alumno aprobado")
+#fin_if
+
